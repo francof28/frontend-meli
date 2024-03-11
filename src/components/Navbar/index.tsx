422ff016082bd269c8ca4583f7e1dded1ceb8deb
Franco Fatala <ffatala28@gmail.com>
2024-03-11 20:10:09 -0300

    feat: Navbar design first approach

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,1 +1,27 @@
-export const Navbar = () => <></>
+import './navbar.scss'
+
+export const Navbar = () => {
+
+    return (
+        <header className='navbar'>
+            <div className='container__search'>
+                <img className='logo__meli' src='assets/Logo_ML2x.png' alt='Logo de MercadoLibre'
+                    loading='lazy'
+                    width={50}
+                    height={32}
+                />
+                <input className='input__search' type='text'
+                    placeholder='Nunca dejes de buscar'
+                    aria-label='Buscar en MercadoLibre'
+                />
+                <button className='button__search' type='submit' title='Buscar'>
+                    <img src='assets/ic_Search2x.png' alt='Input logo lupa de búsqueda'
+                        loading='lazy'
+                        width={12}
+                        height={12}
+                    />
+                </button>
+            </div>
+        </header>
+    )
+};

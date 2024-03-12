071c084c8823b4529fa5623244661f261d6e5b73
Franco Fatala <ffatala28@gmail.com>
2024-03-12 12:07:24 -0300

    fix: Navbar img paths

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ export const Navbar = () => {
     return (
         <header className='navbar'>
             <div className='container__search'>
-                <img className='logo__meli' src='assets/Logo_ML2x.png' alt='Logo de MercadoLibre'
+                <img className='logo__meli' src='/assets/Logo_ML2x.png' alt='Logo de MercadoLibre'
                     loading='lazy'
                     width={50}
                     height={32}
@@ -15,7 +15,7 @@ export const Navbar = () => {
                     aria-label='Buscar en MercadoLibre'
                 />
                 <button className='button__search' type='submit' title='Buscar'>
-                    <img src='assets/ic_Search2x.png' alt='Input logo lupa de búsqueda'
+                    <img src='/assets/ic_Search2x.png' alt='Input logo lupa de búsqueda'
                         loading='lazy'
                         width={12}
                         height={12}

5759be3c122e1f205b075a5eb56d68f3d08235f2
Franco Fatala <ffatala28@gmail.com>
2024-03-14 16:44:42 -0300

    feat: mobile design tweaks

--- client/src/components/Navbar/index.tsx
+++ client/src/components/Navbar/index.tsx
@@ -10,17 +10,19 @@ export const Navbar = () => {
                     width={50}
                     height={32}
                 />
-                <input className='input__search' type='text'
-                    placeholder='Nunca dejes de buscar'
-                    aria-label='Buscar en MercadoLibre'
-                />
-                <button className='button__search' type='submit' title='Buscar'>
-                    <img src='/assets/ic_Search2x.png' alt='Input logo lupa de búsqueda'
-                        loading='lazy'
-                        width={12}
-                        height={12}
+                <form className='container__input'>
+                    <input className='input__search' type='text'
+                        placeholder='Nunca dejes de buscar'
+                        aria-label='Buscar en MercadoLibre'
                     />
-                </button>
+                    <button className='button__search' type='submit' title='Buscar'>
+                        <img src='/assets/ic_Search2x.png' alt='Input logo lupa de búsqueda'
+                            loading='lazy'
+                            width={12}
+                            height={12}
+                        />
+                    </button>
+                </form>
             </div>
         </header>
     )
--- client/src/pages/Results/components/Item/Item.tsx
+++ client/src/pages/Results/components/Item/Item.tsx
@@ -5,12 +5,12 @@ export const Item = () => {
         <div className="item__container">
             <img className='item__image' src='https://www.cronista.com/files/image/458/458270/62631239f36c9.jpg' />
             <div className='item__description'>
-                <h3>$ 1.980</h3>
+                <div className='item__location'>
+                    <h3>$ 1.980</h3>
+                    <p>Capital federal</p>
+                </div>
                 <p>Apple ipod touch 6gb negro igual a nuevo</p>
                 <p>Completo unico</p>
-            </div>
-            <div className='item__location'>
-                <p>Capital federal</p>
             </div>
         </div>
     )
